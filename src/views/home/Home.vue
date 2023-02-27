be112de4efefe03a220a1d0c9e6ff667e1f09a04
Chen <tempnoemail@outlook.com>
2023-02-27 16:04:28 +0800

    主页框架

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,11 +1,44 @@
 <script setup lang='ts'>
 import { ref, reactive } from 'vue'
+let categories = reactive({
+    home: '推荐',
+    douga: '动画',
+    game: '游戏',
+    cinephile: '影视',
+    life: '生活',
+    interest: '兴趣'
+})
 </script>
 
 <template>
     <div class="home-body">
+        <div class="home-content">
+            <div class="left">
+            </div>
+            <div class="content">
+                
+            </div>
+            <div class="right">
+
+            </div>
+        </div>
     </div>
 </template>
 
 <style scoped>
+.home-content {
+    width: 1160px;
+    margin: 0 auto;
+    display: flex;
+}
+.left {
+    width: 180px;
+}
+.content {
+    width: 680px;
+}
+.right {
+    width: 260px;
+}
+
 </style>
